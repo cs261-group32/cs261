a2db6f62bf40ce17a66021b32424cbe41f8a338f
Nok Hang Lo <bluetotkc@gmail.com>
2026-02-19 17:44:43 +0000

    feat(airport-core): started to work on runway, takeoffqueue classes

--- src/main/java/com/group32/cs261project/model/Runway.java
+++ src/main/java/com/group32/cs261project/model/Runway.java
@@ -1,5 +1,91 @@
 package com.group32.cs261project.model;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import com.group32.cs261project.model.enums.RunwayMode;
+import com.group32.cs261project.model.enums.RunwayStatus;
+
 public class Runway {
+
+    private final int runwayNumber;
+    private final int lengthM;
+    private final int bearingDeg;
+    private RunwayMode mode;
+    private RunwayStatus status;
+    private LocalDateTime occupiedUntil;
+
+    public Runway(
+        int runwayNumber, 
+        int lengthM, 
+        int bearingDeg, 
+        RunwayMode mode,
+        RunwayStatus status
+    ) {
+        this.runwayNumber = Objects.requireNonNull(runwayNumber);
+        this.lengthM = Objects.requireNonNull(lengthM);
+        this.bearingDeg = Objects.requireNonNull(bearingDeg);
+        this.mode = Objects.requireNonNull(mode);
+        this.status = Objects.requireNonNull(status);
+        this.occupiedUntil = null;
+    }
+
+    public boolean isAvailableForLanding(LocalDateTime time) {
+        return false;
+    }
+
+    public boolean isAvailableForTakeOff(LocalDateTime time) {
+        return false;
+    }
+
+    public void occupyUntil(LocalDateTime time) {
+
+    }
+
+    public int runwayNumber() {
+        return this.runwayNumber;
+    }
+
+    public int lengthM() {
+        return this.lengthM;
+    }
+
+    public int bearingDeg() {
+        return this.bearingDeg;
+    }
+
+    public RunwayMode mode() {
+        return this.mode;
+    }
+
+    public RunwayStatus status() {
+        return this.status;
+    }
+
+    public LocalDateTime occupiedUntil() {
+        return this.occupiedUntil;
+    }
+    
+    public boolean isOccupied(LocalDateTime time) {
+        return false;    
+    }
+    
+    public void clearOccupancy() {
+
+    }
+
+    public LocalDateTime nextAvailableTime(LocalDateTime time) {
+        return null;
+    }
+
+    public void setMode(RunwayMode newMode) {
+        this.mode = newMode;
+    }
+
+    public void setStatus(RunwayStatus newStatus) {
+        this.status = newStatus;
+    }
+
+
     
 }
--- src/main/java/com/group32/cs261project/queues/TakeoffQueue.java
+++ src/main/java/com/group32/cs261project/queues/TakeoffQueue.java
@@ -1,5 +1,38 @@
 package com.group32.cs261project.queues;
 
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.group32.cs261project.model.Aircraft;
+
 public class TakeoffQueue {
+
+    private Queue<Aircraft> queue;
+
+    public TakeoffQueue() {
+        this.queue = new LinkedList<>();
+
+    }
+
+    public void enqueue(Aircraft a, LocalDateTime t) {
+
+    }
+
+    public Aircraft peek() {
+        return null;
+    }
+
+    public Aircraft dequeue() {
+        return null;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
     
 }
