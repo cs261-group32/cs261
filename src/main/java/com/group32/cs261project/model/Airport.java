84819a007ef72cc554b2a28a94bb1b52f5c613a7
Nok Hang Lo <bluetotkc@gmail.com>
2026-02-26 20:34:23 +0000

    feat(runway, airport): filled out more methods, added comments

--- src/main/java/com/group32/cs261project/model/Airport.java
+++ src/main/java/com/group32/cs261project/model/Airport.java
@@ -1,7 +1,6 @@
 package com.group32.cs261project.model;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import com.group32.cs261project.model.enums.RunwayMode;
@@ -9,47 +8,101 @@ import com.group32.cs261project.model.enums.RunwayStatus;
 import com.group32.cs261project.queues.HoldingPattern;
 import com.group32.cs261project.queues.TakeoffQueue;
 
+/**
+ * Class for Airport
+ */
 public class Airport {
 
     private final List<Runway> runways;
     private final HoldingPattern holdingPattern;
     private final TakeoffQueue takeoffQueue;
 
+    /**
+     * Constructor
+     * @param runways A list of runway objects
+     */
     public Airport(List<Runway> runways) {
         this.runways = runways;
         this.holdingPattern = new HoldingPattern();
         this.takeoffQueue = new TakeoffQueue();
     }
 
+    /**
+     * Getter for list of runways
+     * @return list of runways
+     */
     public List<Runway> runways() {
         return this.runways;
     }
 
+    /**
+     * Getter for holding pattern
+     * @return holding pattern object
+     */
     public HoldingPattern holdingPattern() {
         return this.holdingPattern;
     }
 
+    /**
+     * Getter for take off queue
+     * @return takeoff queue object
+     */
     public TakeoffQueue takeoffQueue() {
         return this.takeoffQueue;
     }
 
+    /**
+     * Gets runway of given runway number in the airport
+     * @param runwayNumber runway number
+     * @return corresponding runway at the airport
+     */
     public Runway getRunway(int runwayNumber) {
         return this.runways.get(runwayNumber);
     }
 
+    /**
+     * Set status of runway with given runway number
+     * @param runwayNumber runway number
+     * @param status one of LANDING, TAKEOFF, MIXED 
+     */
     public void setRunwayStatus(int runwayNumber, RunwayStatus status) {
         this.getRunway(runwayNumber).setStatus(status);
     }
 
+    /**
+     * Set mode of runway with given runway number
+     * @param runwayNumber runway number
+     * @param mode one of AVAILABLE, INSPECTION, SNOW_CLEARANCE, EQUIPMENT_FAILURE
+     */
     public void setRunwayMode(int runwayNumber, RunwayMode mode) {
         this.getRunway(runwayNumber).setMode(mode);
     }
 
+    /**
+     * Finds an available landing runway for landing at the given time
+     * @param time datetime to check
+     * @return an available runway or null if none available
+     */
     public Runway findAvailableLandingRunway(LocalDateTime time) {
+        for (Runway runway : this.runways) {
+            if (runway.isAvailableForLanding(time)) {
+                return runway;
+            }
+        }
         return null;
     }
 
+    /**
+     * Finds an available runway for takeoff at the given time
+     * @param time datetime to check
+     * @return an available runway or null if none available
+     */
     public Runway findAvailableTakeoffRunway(LocalDateTime time) {
+        for (Runway runway : this.runways) {
+            if (runway.isAvailableForTakeOff(time)) {
+                return runway;
+            }
+        }
         return null;
     }
 
--- src/main/java/com/group32/cs261project/model/Runway.java
+++ src/main/java/com/group32/cs261project/model/Runway.java
@@ -6,6 +6,9 @@ import java.util.Objects;
 import com.group32.cs261project.model.enums.RunwayMode;
 import com.group32.cs261project.model.enums.RunwayStatus;
 
+/**
+ * Runway class
+ */
 public class Runway {
 
     private final int runwayNumber;
@@ -15,13 +18,15 @@ public class Runway {
     private RunwayStatus status;
     private LocalDateTime occupiedUntil;
 
-    public Runway(
-        int runwayNumber, 
-        int lengthM, 
-        int bearingDeg, 
-        RunwayMode mode,
-        RunwayStatus status
-    ) {
+    /**
+     * Constructor
+     * @param runwayNumber
+     * @param lengthM
+     * @param bearingDeg
+     * @param mode
+     * @param status
+     */
+    public Runway(int runwayNumber, int lengthM, int bearingDeg, RunwayMode mode, RunwayStatus status) {
         this.runwayNumber = Objects.requireNonNull(runwayNumber);
         this.lengthM = Objects.requireNonNull(lengthM);
         this.bearingDeg = Objects.requireNonNull(bearingDeg);
@@ -30,60 +35,125 @@ public class Runway {
         this.occupiedUntil = null;
     }
 
+    /**
+     * Getter for runway number
+     * @return the runway number
+     */
     public int runwayNumber() {
         return this.runwayNumber;
     }
 
+    /**
+     * Getter for runway length
+     * @return runway length in metres
+     */
     public int lengthM() {
         return this.lengthM;
     }
 
+    /**
+     * Getter for bearing of runway
+     * @return bearing in degrees
+     */
     public int bearingDeg() {
         return this.bearingDeg;
     }
 
+    /**
+     * Getter for runway mode
+     * @return one of MIXED, LANDING, TAKEOFF
+     */
     public RunwayMode mode() {
         return this.mode;
     }
 
+    /**
+     * Getter for runway status
+     * @return one of AVAILABLE, INSPECTION, SNOW_CLEARANCE, EQUIPMENT_FAILURE
+     */
     public RunwayStatus status() {
         return this.status;
     }
 
+    /**
+     * Setter for runway mode
+     * @param newMode one of LANDING, TAKEOFF, MIXED
+     */
+    public void setMode(RunwayMode newMode) {
+        this.mode = newMode;
+    }
+
+    /**
+     * Setter for runway status
+     * @param newStatus one of AVAILABLE, INSPECTION, SNOW_CLEARANCE, EQUIPMENT_FAILURE
+     */
+    public void setStatus(RunwayStatus newStatus) {
+        this.status = newStatus;
+    }
+
+    /**
+     * Checks if the runway is available for landing at the given time
+     * @param time time to check
+     * @return true if available
+     */
     public boolean isAvailableForLanding(LocalDateTime time) {
+        if (this.mode == RunwayMode.LANDING || this.mode == RunwayMode.MIXED) {
+            return !this.isOccupied(time);
+        }
         return false;
     }
 
+    /**
+     * Checks if the runway is available for takeoff at the given time
+     * @param time time to check
+     * @return true if available
+     */
     public boolean isAvailableForTakeOff(LocalDateTime time) {
+        if (this.mode == RunwayMode.TAKEOFF || this.mode == RunwayMode.MIXED) {
+            return !this.isOccupied(time);
+        }
         return false;
     }
 
+    /**
+     * Getter for time the runway is occupied until
+     * @return a time if the runway is occupied, null otherwise
+     */
     public LocalDateTime occupiedUntil() {
         return this.occupiedUntil;
     }
 
+    /**
+     * set runway to be occupied until a given time
+     * @param time time to set
+     */
     public void occupyUntil(LocalDateTime time) {
         if (!this.isOccupied(time))
             this.occupiedUntil = time;
     }
     
+    /**
+     * Checks if runway is occupied at a given time
+     * @param time time to check
+     * @return true if occupied
+     */
     public boolean isOccupied(LocalDateTime time) {
         return (this.occupiedUntil == null || this.occupiedUntil.isBefore(time));
     }
     
+    /**
+     * Marks runway is not occupied
+     */
     public void clearOccupancy() {
         this.occupiedUntil = null;
     }
 
+    /**
+     * Gets gets next available time runway is available
+     * @param time current time
+     * @return current time if available, otherwise next available time
+     */
     public LocalDateTime nextAvailableTime(LocalDateTime time) {
         return this.isOccupied(time) ? this.occupiedUntil : time;
-    }
-
-    public void setMode(RunwayMode newMode) {
-        this.mode = newMode;
-    }
-
-    public void setStatus(RunwayStatus newStatus) {
-        this.status = newStatus;
     }
 }
