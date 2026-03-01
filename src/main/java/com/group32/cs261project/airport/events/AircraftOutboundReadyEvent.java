7c2e439c8468555da8f55f7cc1965b91997607a6
Nok Hang Lo <bluetotkc@gmail.com>
2026-03-01 16:39:36 +0000

    feat: wrote FlightSeeder, aircraft generation event classes

--- src/main/java/com/group32/cs261project/airport/events/AircraftOutboundReadyEvent.java
+++ src/main/java/com/group32/cs261project/airport/events/AircraftOutboundReadyEvent.java
@@ -2,24 +2,29 @@ package com.group32.cs261project.airport.events;
 
 import java.time.Instant;
 
+import com.group32.cs261project.model.Aircraft;
 import com.group32.cs261project.model.Airport;
 import com.group32.cs261project.sim.SimulationEngine;
 import com.group32.cs261project.sim.events.Event;
 
 public class AircraftOutboundReadyEvent implements Event<Airport> {
 
-    public AircraftOutboundReadyEvent() {
+    private final Instant time;
+    private final Aircraft aircraft;
 
+    public AircraftOutboundReadyEvent(Instant time, Aircraft aircraft) {
+        this.time = time;
+        this.aircraft = aircraft;
     }
 
     @Override
     public Instant time() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return time;
     }
 
     @Override
     public void handle(SimulationEngine<Airport> engine) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        engine.context().takeoffQueue().enqueue(aircraft, time);
     }
 
 
